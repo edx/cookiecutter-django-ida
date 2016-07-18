7f32ab5a69e3b10ab77da3eea98b6b0b42af87e1
Clinton Blackburn <clinton.blackburn@gmail.com>
2016-07-17 23:18:43 -0400

    Cleaned pylint deprecations

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/apps/core/tests/test_models.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/apps/core/tests/test_models.py
@@ -28,19 +28,19 @@ class UserTests(TestCase):
         """ Test that the user model concatenates first and last name if the full name is not set. """
         full_name = "George Costanza"
         user = G(User, full_name=full_name)
-        self.assertEquals(user.get_full_name(), full_name)
+        self.assertEqual(user.get_full_name(), full_name)
 
         first_name = "Jerry"
         last_name = "Seinfeld"
         user = G(User, full_name=None, first_name=first_name, last_name=last_name)
         expected = "{first_name} {last_name}".format(first_name=first_name, last_name=last_name)
-        self.assertEquals(user.get_full_name(), expected)
+        self.assertEqual(user.get_full_name(), expected)
 
         user = G(User, full_name=full_name, first_name=first_name, last_name=last_name)
-        self.assertEquals(user.get_full_name(), full_name)
+        self.assertEqual(user.get_full_name(), full_name)
 
     def test_unicode(self):
         """Verify that the model's unicode method returns the user's full name."""
         full_name = "Bob"
         user = G(User, full_name=full_name)
-        self.assertEquals(unicode(user), full_name)
+        self.assertEqual(unicode(user), full_name)
